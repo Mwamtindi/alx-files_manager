bb4ae1a713b1679261b682cfa420a99a6ed59f21
Mwamtindi <mwamtindishaban@gmail.com>
2025-02-01 15:35:56 +0300

    auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -28,7 +28,7 @@ class AuthController {
 
     const token = uuidv4();
     const key = `auth_${token}`;
-    await redisClient.set(key, user._id.toString(), 86400);
+    await redisClient.set(key, user._id.toString(), 'EX' 86400);
 
     return res.status(200).json({ token });
   }
