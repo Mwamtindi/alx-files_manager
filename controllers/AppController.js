e83b1392d1a6204d3690a50ecc792d7910f1e485
Mwamtindi <mwamtindishaban@gmail.com>
2025-02-01 10:16:22 +0300

    appcontroller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,20 +4,20 @@ import redisClient from '../utils/redis';
 class AppController {
   static async getStatus(req, res) {
     res.status(200).json({
-			redis: redisClient.isAlive(),
-			db: dbClient.isAlive(),
-		});
-	}
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
 
-	static async getStats(req, res) {
-		const usersCount = await dbClient.nbUsers();
-		const filesCount = await dbClient.nbFiles();
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
 
-			res.status(200).json({
-					users: usersCount,
-					files: filesCount,
-			});
-	}
+    res.status(200).json({
+    users: usersCount,
+    files: filesCount,
+    });
+  }
 }
 
 export default AppController;
