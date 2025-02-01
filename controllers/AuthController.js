27297c90440d5d61c4db4a97602cfc620d31efe6
Mwamtindi <mwamtindishaban@gmail.com>
2025-02-01 12:43:29 +0300

    auth a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+import { redisClient} from '../utils/redis';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    const [email, password] = credentials.split(':');
+
+    if (!email || !password) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const user = await usersCollection.findOne({ email, password: hashedPassword });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    await redisClient.set(key, user._id.toString(), 86400); //validity 24hrs
+
+    return res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    await redisClient.del(key);
+    return res.status(204).send();
+  }
+}
+
+export default AuthController;
